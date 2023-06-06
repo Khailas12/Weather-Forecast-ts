60373d1f4149db492e7b4cb595a6b6ae4f601e9d
Khailas12 <khailas303@gmail.com>
2023-06-07 00:48:31 +0530

    whole updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ForecastComponent } from './forecast/forecast.component';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule,
+    DropdownModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/forecast/forecast.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { ForecastService } from '../services/forecast.service';
+
+@Component({
+  selector: 'app-forecast',
+  templateUrl: './forecast.component.html',
+  styleUrls: ['./forecast.component.css']
+})
+export class ForecastComponent implements OnInit {
+  title = 'weather-app';
+  navigationLinks = ['Home', 'Counter', 'Fetch Data'];
+  tableHeaders = ['Date and Time', 'Temp. (C)', 'Temp. (F)', 'Condition'];
+
+  weatherConditions: any[] = [];
+  selectedCondition: any;
+  forecastData: any[] = [];
+  filteredData: any[] = [];
+  weatherDataError = '';
+  weatherConditionsError = '';
+
+  constructor(private forecastService: ForecastService) { }
+
+  ngOnInit(): void {
+    this.fetchWeatherConditions();
+    this.fetchWeatherData();
+  }
+
+  private fetchWeatherConditions(): void {
+    this.forecastService.getConditions().subscribe({
+      next: (data) => {
+        this.weatherConditions = data?.weatherCondition?.map((condition: any) => condition.condition) || [];
+      },
+      error: () => {
+        this.weatherConditionsError = 'Error fetching weather conditions';
+      }
+    });
+  }
+
+  private fetchWeatherData(): void {
+    this.forecastService.getConditionData().subscribe({
+      next: (forecast) => {
+        this.forecastData = forecast.data;
+        this.filteredData = this.forecastData;
+      },
+      error: () => {
+        this.weatherDataError = 'Error fetching weather data';
+      }
+    });
+  }
+
+  filterData(): void {
+    this.filteredData = this.selectedCondition && this.selectedCondition !== 'All'
+      ? this.forecastData.filter(item => item.condition === this.selectedCondition)
+      : this.forecastData;
+  }
+
+  convertTemperature(temperature: number, unit: 'C' | 'F'): string {
+    if (unit === 'C') {
+      return temperature + '°C';
+    } else if (unit === 'F') {
+      const fahrenheit = (temperature * 9) / 5 + 32;
+      return fahrenheit + '°F';
+    } else {
+      return '';
+    }
+  }
+
+}
